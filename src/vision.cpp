2eb1e2521e665ca031add936011c64085ea5524b
RoyYang0714 <sy2es93104@gmail.com>
2024-07-09 18:23:07 +0200

    feat: Add 3D box IoU.

--- src/vision.cpp
+++ src/vision.cpp
@@ -10,6 +10,7 @@
 #include "box_iou_rotated/box_iou_rotated.h"
 #include "ms_deform_attn/ms_deform_attn.h"
 #include "deform_conv/deform_conv.h"
+#include "iou_box3d/iou_box3d.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("nms_rotated", &nms_rotated);
@@ -33,4 +34,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
       "modulated_deform_conv_backward");
   m.def("ms_deform_attn_forward", &ms_deform_attn_forward, "ms_deform_attn_forward");
   m.def("ms_deform_attn_backward", &ms_deform_attn_backward, "ms_deform_attn_backward");
+  m.def("iou_box3d", &IoUBox3D, "iou_box3d");
 }
